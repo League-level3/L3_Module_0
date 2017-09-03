cead18899321e1ced54c9171786b70ae484e7b6f
100297 <tabitha0528@yahoo.com>
2017-09-03 13:01:05 -0700

    Add files via upload

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,22 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+public class GuestBook implements ActionListener {
+	JFrame a;
+	JPanel pan;
+	JButton add;
+	JButton view;
+	String viewNames;
+	ArrayList<String> names = new ArrayList<String>();
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +25,49 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+public static void main(String[] args) {
+	GuestBook a = new GuestBook();
+			a.activate();
+}
+public void  activate() {
+	names.add("Bob Banders");
+	names.add("Sandy Summers");
+	names.add("Greg Ganders");
+	names.add("Donny Doners");
+	a = new JFrame();
+	a.setVisible(true);
+	pan  = new JPanel();
+	add = new JButton();
+	view = new JButton();
+	a.add(pan);
+	pan.add(add);
+	pan.add(view);
+
+	add.setText("Add Name");
+	view.setText("View Names");
+	add.addActionListener(this);
+	view.addActionListener(this);
+	
+
+
+	a.pack();
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource() == add) {
+		String name = JOptionPane.showInputDialog("Enter a name");
+		names.add(name);
+	}
+	if(e.getSource() ==view ) {
+		for(String s : names) {
+			if(s != null)
+			viewNames = viewNames + s + "\n";
+			
+		}JOptionPane.showMessageDialog(null, viewNames );
+	}
+}
+
+
+
 }
