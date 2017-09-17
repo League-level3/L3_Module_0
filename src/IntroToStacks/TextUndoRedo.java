69bfebcd47283679fc2588fc52869e16db6d73d8
100297 <league@WorthyMini-5.attlocal.net>
2017-09-17 12:55:15 -0700

    fdsdfssfd

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,25 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.nio.charset.Charset;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener{
+	JFrame a;
+	JPanel b;
+	JLabel main;
+	Stack<String> back;
+
+	public static void main(String[] args) {
+		TextUndoRedo main = new TextUndoRedo();
+		main.activate();
+		} 
+	
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +32,47 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+	void activate() {
+		a = new JFrame();
+		b = new 	JPanel();
+		main = new JLabel();
+		a.add(b);
+		b.add(main);
+		a.setVisible(true);
+		a.pack();
+		a.addKeyListener(this);
+		back = new Stack<String>();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char a =	e.getKeyChar();
+			
+		if(e.getKeyCode() == KeyEvent.VK_0) {
+			System.out.println("should Pop");
+			System.out.println(back.pop());
+			main.setText(main.getText() + back.pop());
+		}
+		if(a != KeyEvent.VK_0)	main.setText(main.getText() + a);
+		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+		String s = main.getText();
+		System.out.println("should Delete");
+		back.push(s.substring(s.length()-2));
+		main.setText(s.substring(0, s.length()-2));
+		
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/IntroToStacks/as.java
@@ -0,0 +1,12 @@
+package IntroToStacks;
+
+public class as {
+
+	public static void main(String[] args) {
+		System.out.println("Something");
+		mai();
+	}
+	static void mai() {
+	main(null);
+	}
+}
